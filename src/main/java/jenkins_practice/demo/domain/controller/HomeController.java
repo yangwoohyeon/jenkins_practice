316702bca82cb346ddef33a7a076e6c0e814a924
yangwoohyeon <dngus0717@gmail.com>
2025-05-10 16:28:58 +0900

    Controller 수정

--- src/main/java/jenkins_practice/demo/domain/controller/HomeController.java
+++ src/main/java/jenkins_practice/demo/domain/controller/HomeController.java
@@ -2,8 +2,9 @@ package jenkins_practice.demo.domain.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
     @GetMapping("/")
     public String home() {return "hello";}
