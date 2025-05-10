7222b58c9b4a578f6af04e05f27786da8bf53ba6
yangwoohyeon <dngus0717@gmail.com>
2025-05-10 16:46:26 +0900

    테스트

--- src/main/java/jenkins_practice/demo/domain/controller/HomeController.java
+++ src/main/java/jenkins_practice/demo/domain/controller/HomeController.java
@@ -1,11 +1,12 @@
 package jenkins_practice.demo.domain.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public String home() {return "hello";}
+    public String home() {
+        return "hello123";
+    }
 }
